b41dcd18b379fa892406b70ad4d1ff14ca5a1d13
Stepan Kornev <kornevstepan169@gmail.com>
2024-03-31 20:51:43 +0700

    The load_stock_data management command has been changed

--- app/apps/stocks/management/commands/load_stock_data.py
+++ app/apps/stocks/management/commands/load_stock_data.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from ...tasks import load_available_stocks
+from ...tasks import load_stock_data
 
 
 class Command(BaseCommand):
     help = 'Loads stock data into the database from an external API.'
 
     def handle(self, *args, **options):
-        load_available_stocks.delay()
+        load_stock_data()
