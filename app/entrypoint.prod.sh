84694f82a1198549f672b3acfbc90dde22c45a06
Stepan Kornev <kornevstepan169@gmail.com>
2024-05-19 20:13:20 +0700

    A small change in entrypoint.prod.sh

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -4,7 +4,6 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Postgres еще не запущен..."
 
-    # Проверяем доступность хоста и порта
     while ! nc -z $DB_HOST $DB_PORT; do
       sleep 10
     done
